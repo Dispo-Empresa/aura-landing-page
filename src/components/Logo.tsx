64de3a80fbc8e6cbec14884dbd173e8b73490224
Matheus Teixeira <matheustexeirablu@hotmail.com>
2025-04-21 16:12:08 -0300

    Atualização da logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,11 +3,10 @@ export function Logo() {
   return (
     <div className="text-[#2A98D2] font-outfit font-bold text-2xl flex items-center">
       <img 
-        src="/lovable-uploads/new-logo.png" 
+        src="/lovable-uploads/aura-logo.png" 
         alt="Aura Logo" 
         className="h-10 mr-2" 
       />
-      Aura
     </div>
   );
 }
